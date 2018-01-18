ef4ef03a4b38e4e960c8df032efc781edfdbc56b
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-01-18 11:41:30 +0100

    more routes

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/login', (req, res) => {
   res.end('Here is the login page!');
 });
 
+app.get('*', (req, res) => {
+  res.end('Not Found.');
+});
+
 app.listen(3000, function(){
   console.log('Servidor funcionanado...');
 });
